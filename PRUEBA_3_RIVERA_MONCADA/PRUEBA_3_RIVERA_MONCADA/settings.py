1cedef069290775cd3903c9a0a4ca5727cbbc851
cristobalrivera193 <cantonio1670@gmail.com>
2025-12-24 14:28:03 -0300

    Pequeño arreglo

--- PRUEBA_3_RIVERA_MONCADA/PRUEBA_3_RIVERA_MONCADA/settings.py
+++ PRUEBA_3_RIVERA_MONCADA/PRUEBA_3_RIVERA_MONCADA/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*jn(osaw-pjg=fpl3&)p(#93akfy*p3mgn61h)2pd7gm*x3n@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appTienda',  
+    'appTienda',
+    'rest_framework',  
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
